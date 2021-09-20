851d564bc28eb2d484dd6646c746ed579d433009
Gabe Krahulik <44793856+Lilspartan@users.noreply.github.com>
2021-09-20 01:53:45 +0000

    idk

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Gauge from './Pages/Gauge'
 import Home from './Pages/HomePage'
 import Client from './Pages/Client'
 import PlayerSelect from './Pages/PlayerSelect';
 import {SocketContext, socket} from './context/socket';
+import NewClient from './Pages/NewClient';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route path='/' exact component = {PlayerSelect} />
 					<Route path='/home' exact component = {Home} />
+					<Route path='/newClient' exact component = {NewClient} />
           <Route path='/gauge' exact component = {Gauge} />
           <Route path='/:player' component = {Client} />
         </Switch>
---
+++ src/Pages/NewClient.tsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+
+const Client = () => {
+	const [client, setClient] = useState();
+
+	return (
+		<>
+		
+		</>
+	)
+}
+
+export default Client;
